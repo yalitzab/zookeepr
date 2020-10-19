11f18898430461df0c1a7aec1d18ad3041619c0a
yalitzab <yalitzabueno@gmail.com>
2020-10-18 20:07:16 -0700

    add animal data and post

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 // route so front-end can request data from
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -48,6 +55,33 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JOSON.stringify({ animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+// Validate data
+function validateAnimal(animal){
+    if (!animal.name || typeof animal.name !== 'string'){
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string'){
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
 // route the client to fetch from
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -65,6 +99,19 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+// POST request
+app.post('/api/animals', (req, res) => {
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
